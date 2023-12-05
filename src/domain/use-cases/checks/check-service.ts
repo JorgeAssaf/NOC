97d0b7f77b7bee59e22990b89c1d7df9f6600022
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-05 17:33:42 -0600

    :memo: Add logging functionality to CheckService and
    update server configuration

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity"
+import { LogRepository } from "../../repository/log.repository"
+
 interface CheckServiceUseCase {
   execute: (url: string) => Promise<boolean>
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
+    private logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback,
   ) { }
@@ -17,9 +21,12 @@ export class CheckService implements CheckServiceUseCase {
       if (!response.ok) {
         throw new Error(`Error on check service ${url}`)
       }
+      const log = new LogEntity(url, LogSeverityLevel.low)
+      this.logRepository.saveLog(log)
       this.successCallback()
       return true
     } catch (error) {
+      this.logRepository.saveLog(new LogEntity(`${url} is not ok ${error}`, LogSeverityLevel.high))
       this.errorCallback(`${error}`)
       return false
     }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,16 +1,22 @@
 import { CheckService } from '../domain/use-cases/checks/check-service'
+import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource'
+import { LogRepositoryImpl } from '../infrastructure/repository/log.repository.impl'
 import { CronService } from './cron/cron-service'
 
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDatasource(),
+)
 export class Server {
   public static start(): void {
     console.log('Server is running')
 
-    CronService.createJob('*/5 * * * * *', () => {
+    CronService.createJob('*/10 * * * * *', () => {
       // new CheckService().execute('http://localhost:3000')
       new CheckService(
+        fileSystemLogRepository,
         () => console.log('Success'),
         (error) => console.log(error),
-      ).execute('https://google.com')
+      ).execute('https://www.google.com')
     })
   }
 }
