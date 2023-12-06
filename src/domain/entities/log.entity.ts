eebc14446e4e09b26dfcb26912315bfc4c5de7c0
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-06 13:34:21 -0600

    :recycle: Refactor LogEntity constructor to accept options object

--- src/domain/entities/log.entity.ts
+++ src/domain/entities/log.entity.ts
@@ -4,21 +4,33 @@ export enum LogSeverityLevel {
   high = 'high',
 }
 
+export interface LogEntityOptions {
+  level: LogSeverityLevel
+  message: string
+  origin: string
+  createdAt?: Date
+}
 export class LogEntity {
-  public level: LogSeverityLevel
   public message: string
-  public createAt: Date
-
-  constructor(message: string, level: LogSeverityLevel) {
+  public level: LogSeverityLevel
+  public createdAt: Date
+  public origin: string
+  constructor(options: LogEntityOptions) {
+    const { message, level, origin, createdAt = new Date() } = options
     this.message = message
     this.level = level
-    this.createAt = new Date()
+    this.createdAt = createdAt
+    this.origin = origin
   }
 
   static fromJson(json: string): LogEntity {
-    const { message, level, createAt } = JSON.parse(json)
-    const log = new LogEntity(message, level)
-    log.createAt = new Date(createAt)
+    const { message, level, createdAt, origin } = JSON.parse(json)
+    const log = new LogEntity({
+      message,
+      level,
+      createdAt,
+      origin,
+    })
     return log
   }
 }
