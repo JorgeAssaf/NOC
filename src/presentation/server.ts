b96ebbfabfe3d6ef1b3b337a7fa9cbd5e55da0d3
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-06 13:40:24 -0600

    :memo: Add email service and send log email feature

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,22 +1,27 @@
 import { CheckService } from '../domain/use-cases/checks/check-service'
+import { SendLogEmail } from '../domain/use-cases/email/send-email-logs'
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource'
 import { LogRepositoryImpl } from '../infrastructure/repository/log.repository.impl'
 import { CronService } from './cron/cron-service'
+import { EmailService } from './email/email.service'
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
 )
+const emailService = new EmailService()
 export class Server {
   public static start(): void {
     console.log('Server is running')
 
-    CronService.createJob('*/10 * * * * *', () => {
-      // new CheckService().execute('http://localhost:3000')
-      new CheckService(
-        fileSystemLogRepository,
-        () => console.log('Success'),
-        (error) => console.log(error),
-      ).execute('https://www.google.com')
-    })
+    //TODO seend email
+    // const sendEmailLogs = new SendLogEmail(fileSystemLogRepository, emailService)
+    // sendEmailLogs.execute(['jorgeassaf799@gmail.com'])
+    // const emailService = new EmailService(fileSystemLogRepository)
+    // emailService.sendEmailWithLogs('Jorgeassaf799@gmail.com')
+    // CronService.createJob('*/10 * * * * *', () => {
+    //   new CheckService(
+    //     fileSystemLogRepository,
+    //   ).execute('https://www.google.com')
+    // })
   }
 }
