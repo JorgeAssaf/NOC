a64f7084db63b53d265e699b0f0f6aa32896a00d
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-06 13:37:19 -0600

    :recycle: Refactor CheckService class

--- src/domain/use-cases/checks/check-service.ts
+++ src/domain/use-cases/checks/check-service.ts
@@ -1,18 +1,18 @@
-import { LogEntity, LogSeverityLevel } from "../../entities/log.entity"
-import { LogRepository } from "../../repository/log.repository"
+import { LogEntity, LogSeverityLevel } from '../../entities/log.entity'
+import { LogRepository } from '../../repository/log.repository'
 
 interface CheckServiceUseCase {
   execute: (url: string) => Promise<boolean>
 }
 
-type SuccessCallback = () => void
-type ErrorCallback = (error: string) => void
+type SuccessCallback = (() => void) | undefined
+type ErrorCallback = ((error: string) => void) | undefined
 
 export class CheckService implements CheckServiceUseCase {
   constructor(
     private logRepository: LogRepository,
-    private readonly successCallback: SuccessCallback,
-    private readonly errorCallback: ErrorCallback,
+    private readonly successCallback?: SuccessCallback,
+    private readonly errorCallback?: ErrorCallback,
   ) { }
 
   public async execute(url: string): Promise<boolean> {
@@ -21,13 +21,23 @@ export class CheckService implements CheckServiceUseCase {
       if (!response.ok) {
         throw new Error(`Error on check service ${url}`)
       }
-      const log = new LogEntity(url, LogSeverityLevel.low)
+      const log = new LogEntity({
+        message: `${url} is ok`,
+        level: LogSeverityLevel.low,
+        origin: 'CheckService',
+      })
       this.logRepository.saveLog(log)
-      this.successCallback()
+      this.successCallback && this.successCallback()
       return true
     } catch (error) {
-      this.logRepository.saveLog(new LogEntity(`${url} is not ok ${error}`, LogSeverityLevel.high))
-      this.errorCallback(`${error}`)
+      this.logRepository.saveLog(
+        new LogEntity({
+          message: `${url} is not ok ${error}`,
+          level: LogSeverityLevel.high,
+          origin: 'CheckService',
+        }),
+      )
+      this.errorCallback && this.errorCallback(`${url} is not ok ${error}`)
       return false
     }
   }
