e8dd8d83accc0ceb053c0f7fd23a95a53a7b1b32
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-07 11:03:26 -0600

    :recycle: Refactor server.ts to include new log repositories and check service

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,26 +1,41 @@
-import { CheckService } from '../domain/use-cases/checks/check-service'
-import { SendLogEmail } from '../domain/use-cases/email/send-email-logs'
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource'
+import { PostgresLogDatasource } from '../infrastructure/datasources/postgress.datasource'
 import { LogRepositoryImpl } from '../infrastructure/repository/log.repository.impl'
-import { CronService } from './cron/cron-service'
 import { EmailService } from './email/email.service'
+import { CronService } from './cron/cron-service'
+import { CheckServiceMultiple } from '../domain/use-cases/checks/check-service-multiple'
+import { MongoDatasource } from '../infrastructure/datasources/mongo.datasource'
 
-const fileSystemLogRepository = new LogRepositoryImpl(
-  new FileSystemDatasource(),
-)
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDatasource())
+
+const postgresLogRepository = new LogRepositoryImpl(new PostgresLogDatasource())
+
+const mongoLogRepository = new LogRepositoryImpl(new MongoDatasource())
+
 const emailService = new EmailService()
 export class Server {
-  public static start(): void {
+  public static async start() {
     console.log('Server is running')
 
+
+    // const createLog = await prisma.logs.create({
+    //   data: {
+    //     message: 'test',
+    //     level: LevelSeverity.low,
+    //     origin: 'App.ts',
+    //   },
+    // })
     //TODO seend email
     // const sendEmailLogs = new SendLogEmail(fileSystemLogRepository, emailService)
     // sendEmailLogs.execute(['jorgeassaf799@gmail.com'])
     // const emailService = new EmailService(fileSystemLogRepository)
     // emailService.sendEmailWithLogs('Jorgeassaf799@gmail.com')
+
     // CronService.createJob('*/10 * * * * *', () => {
-    //   new CheckService(
-    //     fileSystemLogRepository,
+    //   new CheckServiceMultiple(
+    //     [fileSystemLogRepository, mongoLogRepository, postgresLogRepository],
+    //     () => console.log('success'),
+    //     (error) => console.log(error),
     //   ).execute('https://www.google.com')
     // })
   }
