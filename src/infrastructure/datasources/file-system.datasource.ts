d80040d04719965a1664eea54400024fd6b6fc8e
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-07 11:04:03 -0600

    :bug: Fix file paths in FileSystemDatasource

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -3,10 +3,10 @@ import { LogDatasource } from '../../domain/datasources/log.datasource'
 import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
 
 export class FileSystemDatasource implements LogDatasource {
-  private readonly logPath = 'logs/'
-  private readonly allLogsPath = `${this.logPath}logs-all.log`
-  private readonly mediumLogsPath = `${this.logPath}logs-medium.log`
-  private readonly highLogsPath = `${this.logPath}logs-high.log`
+  private readonly logPath = 'logs/';
+  private readonly allLogsPath = 'logs/logs-all.log';
+  private readonly mediumLogsPath = 'logs/logs-medium.log';
+  private readonly highLogsPath = 'logs/logs-high.log';
 
   constructor() {
     this.createLogFiles()
@@ -26,7 +26,8 @@ export class FileSystemDatasource implements LogDatasource {
   }
 
   private getLogsFromFile = (path: string): LogEntity[] => {
-    const content = fs.readFileSync(path, 'utf-8')
+    const content = fs.readFileSync(path, { encoding: 'utf-8' })
+    if (content === '') return []
     const logs = content.split('\n').map(LogEntity.fromJson)
     return logs
   }
