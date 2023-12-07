d542f2ceaf0911381e617e9488132e05a5d613ba
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-07 11:02:06 -0600

    :sparkles: Add MongoDB connection and import envs plugin

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,13 @@
+import { envs } from './config/plugins/envs.plugin'
+import { MongoDataBase } from './data/mongo/init'
 import { Server } from './presentation/server'
 
-function main() {
+async function main() {
+  await MongoDataBase.connect({
+    dbName: envs.MONGO_DB_NAME,
+    url: envs.MONGO_URL,
+  })
+
   Server.start()
 }
 
