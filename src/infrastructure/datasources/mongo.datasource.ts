0225c65794e9ed8bdb2174f0b1db161d54818ec3
Jorge Assaf <jorgeassaf160@gmail.com>
2023-12-07 11:03:40 -0600

    :sparkles: Add MongoDB and Postgres datasources

---
+++ src/infrastructure/datasources/mongo.datasource.ts
@@ -0,0 +1,13 @@
+import { logModel } from '../../data/mongo'
+import { LogDatasource } from '../../domain/datasources/log.datasource'
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
+
+export class MongoDatasource implements LogDatasource {
+  async saveLog(log: LogEntity): Promise<void> {
+    await logModel.create(log)
+  }
+  async getLogs(severity: LogSeverityLevel): Promise<LogEntity[]> {
+    const logs = await logModel.find({ level: severity })
+    return logs.map((log) => LogEntity.fromObject(log))
+  }
+}
---
+++ src/infrastructure/datasources/postgress.datasource.ts
@@ -0,0 +1,26 @@
+import { PrismaClient } from '@prisma/client'
+import { LogDatasource } from '../../domain/datasources/log.datasource'
+import { LogEntity, LogSeverityLevel } from '../../domain/entities/log.entity'
+
+
+const prisma = new PrismaClient()
+
+export class PostgresLogDatasource implements LogDatasource {
+
+  async saveLog(log: LogEntity): Promise<void> {
+    await prisma.logs.create({
+      data: {
+        ...log
+      },
+    })
+  }
+  async getLogs(severity: LogSeverityLevel): Promise<LogEntity[]> {
+    const logs = await prisma.logs.findMany({
+      where: {
+        level: severity,
+      },
+    })
+
+    return logs.map((log) => LogEntity.fromObject(log))
+  }
+}
